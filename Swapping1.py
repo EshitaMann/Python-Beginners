bfa63053048d1ad0e228e8a6eb033460f65ee48d
Eshita Mann <eshita.mann100@gmail.com>
2017-10-04 23:55:24 +0530

    Add files via upload

---
+++ Swapping1.py
@@ -0,0 +1,12 @@
+#Swapping of two no.s(using third variable)
+
+a=int(input('Enter 1st no:'))
+b=int(input('Enter 2nd no:'))
+print('First no:',a)
+print('Second no:',b)
+c=a
+a=b
+b=c
+print('After Swapping')
+print('1st no:',a)
+print('2nd no:',b)
