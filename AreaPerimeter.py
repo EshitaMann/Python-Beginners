bbd9d7fd4f8b8a47eca21af03fed96ffa001dd99
Eshita Mann <eshita.mann100@gmail.com>
2017-10-04 23:59:06 +0530

    Add files via upload

---
+++ AreaPerimeter.py
@@ -0,0 +1,14 @@
+#Area and perimeter of circle and rectangle
+
+r=float(input('Enter radius:'))
+A=3.14*r*r
+P=2*3.14*r
+print('Area of circle:',A)
+print('Perimeter of circle:',P)
+l=float(input('Enter lenght:'))
+b=float(input('Enter breadth:'))
+a=l*b
+p=2*(l+b)
+print("---For rectangle---")
+print('Area:',a)
+print('Perimeter:',b)
